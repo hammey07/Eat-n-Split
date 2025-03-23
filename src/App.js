26a9aa86ba9561e607459dcfcb191eca021e50c7
hammey07 <hammey07.hs@gmail.com>
2025-03-23 00:53:52 +0000

    issue with split form fixed

--- src/App.js
+++ src/App.js
@@ -73,6 +73,7 @@ export default function App() {
       </div>
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
